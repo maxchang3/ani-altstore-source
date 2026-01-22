1bb61d7e4b0254cae1e9f77415ec169907c4ebb5
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2026-01-22 18:10:06 +0800

    chore: update output filename and add workflow

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This is a custom AltStore source for [Animeko](https://myani.org), an all-in-one
 
 此项目为 [Animeko](https://myani.org) 的 AltStore 源，Animeko 一站式在线弹幕追番平台。
 
+
+
 ## License
 
 [MIT](./LICENSE) License © [maxchang3](https://github.com/maxchang3)
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { generateSource } from './generator'
 
 console.log('正在生成 Animeko AltStore source...')
 
-const outputPath = 'generated/source.json'
+const outputPath = 'generated/apps.json'
 await fs.mkdir('generated', { recursive: true })
 
 const source = await generateSource()
@@ -11,7 +11,7 @@ const json = JSON.stringify(source, null, 2)
 
 await fs.writeFile(outputPath, json, 'utf8')
 
-console.log(`成功生成 source.json`)
+console.log(`成功生成 apps.json`)
 console.log(`文件已保存到: ${outputPath}`)
 console.log(`包含 ${source.apps[0].versions.length} 个版本`)
 
